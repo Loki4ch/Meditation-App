2d4ed4b9a3f1166b64a20889d77b720767a952dc
Egor Philippov <goshaxp0@gmail.com>
2022-09-22 00:06:02 +0300

    server post in prog

--- src/api/meditationsApi.js
+++ src/api/meditationsApi.js
@@ -2,4 +2,4 @@ import fakeServerInstance from "./instance.js";
 
 export const fetchMeditationsList = () => fakeServerInstance.get("/meditations");
 
-export const postMeditation = (data) => fakeServerInstance.post("/meditations", {data}, {headers: {"Content-Type": "application/json"}});
+export const postMeditation = (data) => fakeServerInstance.post("/meditations", {data});
