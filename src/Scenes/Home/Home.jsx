8cf13cedac585b0be976731eea489d48ecee464e
Egor Philippov <goshaxp0@gmail.com>
2022-10-19 22:57:33 +0300

    commit new

--- src/Scenes/Home/Home.jsx
+++ src/Scenes/Home/Home.jsx
@@ -40,18 +40,6 @@ const Home = (props) => {
         fetchData();
     }, [cardsList.length, editRenderTrigger])
 
-    const checkListEmpty = () => {
-        if (cardsList === undefined) return <Spinner/>;
-    }
-
-    const checkListLength = () => {
-        if (!cardsList.length) return <div className={'alert-text'}>No meditations yet</div>;
-    }
-
-    const editMeditation = (index, newName) => {
-
-    }
-
     const deleteMeditation = (id, index) => {
         removeMeditation(id);
         const newCardsList = [...cardsList];
@@ -61,20 +49,21 @@ const Home = (props) => {
         setCardsList(newCardsList);
     }
 
-const goodCallback = useCallback(deleteMeditation, [cardsList.length]);
+    const getMeditationCards = () => {
+        if (cardsList === undefined) return <Spinner/>;
+        if (cardsList.length === 0) return <div className={'alert-text'}>No meditations yet</div>;
+        return (
+        cardsList.map((card, index) => (
+            <MeditationCard key={index} id={card.id} cardIndex={index} name={card.name} duration={card.duration} description={card.description} daytime={card.daytime} deleteMeditation={goodCallback} cardsList={cardsList} setCardsList={setCardsList}/>
+        )))
+    }
 
-    console.log('home rendered');
+    const goodCallback = useCallback(deleteMeditation, [cardsList.length]);
 
     return (
         <StyledHome>
             <div className={'main-wrapper'}>
-                {checkListEmpty()}
-                {checkListLength()}
-                {(cardsList && !!cardsList.length) &&
-                    cardsList.map((card, index) => (
-                         <MeditationCard key={index} id={card.id} cardIndex={index} name={card.name} duration={card.duration} description={card.description} daytime={card.daytime} deleteMeditation={goodCallback} editMeditation={editMeditation} cardsList={cardsList} setCardsList={setCardsList}/>
-                    ))
-                }
+                {getMeditationCards()}
                 <AddMeditationBtn cardsList={cardsList} setCardsList={setCardsList}/>
             </div>
         </StyledHome>
