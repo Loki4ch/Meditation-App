8a530cfc5bfbd43eb1bc80ad2937f524b28b9c4c
Loki4ch <98488167+Loki4ch@users.noreply.github.com>
2022-11-03 13:01:19 +0300

    dail

--- webpack.config.js
+++ webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
     },
     devtool: "source-map",
     devServer: {
-        port: 4000,
+        port: 3000,
         hot: true,
         historyApiFallback: {index: "/"},
         open: true,
